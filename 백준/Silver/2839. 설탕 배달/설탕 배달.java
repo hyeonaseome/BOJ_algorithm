414e06660f2cc6d9604016e09b7366959ecc5653
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-07 14:31:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(in.readLine());
+		int copyN = N;
+		int count = 0;
+		int quotientFIve = N / 5;
+
+		for (int i = quotientFIve ; i >= 0 ; i--) {
+			int num = N - 5*i;
+			if (num% 3 == 0) {
+				count = i + num/3;
+				break;
+			}
+		}
+		
+		if (count <= 0 ) {
+			count = -1;
+		}
+		
+		sb.append(count);
+		System.out.println(sb);
+	}
+}

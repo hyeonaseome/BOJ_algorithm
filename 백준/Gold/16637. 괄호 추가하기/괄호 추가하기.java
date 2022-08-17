7a94306fff4d9ec11c2797fdb8753a6035557225
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-18 03:13:09 +0900

    [Gold IV] Title: 괄호 추가하기, Time: 128 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
@@ -13,13 +12,8 @@ public class Main {
 	static String line;
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-		// 결과를 한 번에 출력하기 위한 StringBuilder
 		StringBuilder sb = new StringBuilder();
 
-		/**
-		 * 1. 입력 파일 객체화
-		 */
 		int N = Integer.parseInt(in.readLine());
 		isSelected = new boolean[N+1];
 		
@@ -28,18 +22,13 @@ public class Main {
 		
 		max = Integer.MIN_VALUE;
 		
-		/**
-		 * 2. 알고리즘 풀기
-		 */
 		int size = (N + 1) /4;
 		for (int i = 1; i <= size; i++) {
 			R = 2 * i;
 			numbers = new int [R];
 			comb(0, 0);
 		}
-		/**
-		 * 3. 정답 출력
-		 */
+
 		if (max == Integer.MIN_VALUE) {
 			max = line.charAt(0) - '0';
 		}

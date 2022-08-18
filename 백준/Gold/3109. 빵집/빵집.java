eff1835a2daad80eccca07e9f0c3c41684790d81
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-19 06:33:39 +0900

    [Gold II] Title: 빵집, Time: 428 ms, Memory: 43908 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,70 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static int R,C;
-    private static char[][] map;
-    private static int[] dir = {-1,0,1};//오른쪽 라인을 검사해주기 위함
-    private static int width,answer;
+	private static int R, C, count;
+	private static char[][] map;
 
-	public static void main(String[] args) {
+	// 우상, 우, 우하
+	private static int[] dx = {-1, 0, 1};
+	private static int[] dy = {1, 1, 1};
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 
-        R = sc.nextInt();
-        C = sc.nextInt();
+		String[] split = in.readLine().split(" ");
+		R = Integer.parseInt(split[0]);
+		C = Integer.parseInt(split[1]);
+		count = 0;
 
-        sc.nextLine();
+		map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			map[i] = in.readLine().toCharArray();
+		}
 
-        map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			getpipe(i, 0);
+		}
 
-        for (int i = 0; i < R; i++) {
-
-            map[i] = sc.nextLine().toCharArray();
-
-        }
-
-        width = C;
-        answer = 0;
-
-        //윗줄부터 R번째 줄까지 라인 검사
-        for (int i = 0; i < R; i++) dfs(i,0);
-
-        System.out.println(answer);
-
+		sb.append(count);
+		System.out.println(sb);
 	}
-
-    //라인 검사
-	public static boolean dfs(int x, int y) {
-
-	    //끝 라인까지 왔을 때
-        if (y == width-1) {
-
-            answer++;//연결 갯수를 추가
-            return true;
-        }
-
-        //오른쪽 라인의 대각선 위, 옆,대각선 아래를 검사
-        for (int i = 0; i < 3; i++) {
-
-            int nx = x + dir[i];
-            int ny = y + 1;
-
-            //범위 밖
-            if (nx < 0 || ny < 0 || nx == R || ny == C) continue;
-
-            //못지나는 곳
-            if (map[nx][ny] == 'x') continue;
-
-            //방문했음을 표시
-            map[nx][ny] = 'x';
-            
-            //다음 라인 검사
-            //라인의 가지치기를 방지하기 위해 true를 리턴한다.
-            //true를 리턴하면 다음 분기문으로 넘어가지 않는다.
-            if (dfs(nx,ny)) return true;
-
-        }
-
-        return false;
-    }
+	private static boolean getpipe(int x, int y) {
+		// 기저 부분
+		if (y == C - 1) {
+			count++;
+			return true;
+		}
+		
+		// 유도부분
+		for (int i = 0; i < 3; i++) {
+			int newX = x + dx[i];
+			int newY = y + dy[i];
+			if(!(newX >= 0 && newX < R  && newY >= 0  && newY < C)) continue;
+			if (map[newX][newY] == 'x') continue;
+			map[x][y] = 'x';
+			if (getpipe(newX, newY)) return true;
+		}		
+		return false;
+	}
 }

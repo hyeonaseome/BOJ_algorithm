6f9cdd30279c673ccd17fad17b3ab0759bdc4abd
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-20 00:22:49 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 300 ms, Memory: 37684 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(in.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if (X == A) {
+				X = B;
+			}
+			else if (X == B) {
+				X = A;
+			}
+		}
+		System.out.println(X);
+	}
+	
+}

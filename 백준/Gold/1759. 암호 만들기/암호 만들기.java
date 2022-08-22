3217e5bc78e0b21678c56d715eec386129f811cc
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-22 23:38:37 +0900

    [Gold V] Title: 암호 만들기, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -5,14 +5,13 @@ import java.util.Arrays;
 public class Main {
 	private static int L, C;
 	private static char[] alphabet, code;
-
 	private static StringBuilder sb;
+    
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 
 		String[] split = in.readLine().split(" ");
-		// 3 ≤ L ≤ C ≤ 15
 		L = Integer.parseInt(split[0]);
 		C = Integer.parseInt(split[1]);
 
@@ -41,8 +40,8 @@ public class Main {
 		// 유도부분
 		for(int i = start; i < C; i++) {
 			code[cnt] = alphabet[i];
-			if(alphabet[i] == 'a'||alphabet[i] == 'e'||alphabet[i] == 'i'||
-					alphabet[i] == 'o'||alphabet[i] == 'u') {
+			char c = code[cnt];
+			if(c == 'a'||c == 'e'||c == 'i'|| c == 'o'||c== 'u') {
 				comb(cnt+1, i+1, vowelCnt + 1);
 			}else {
 				comb(cnt+1, i+1, vowelCnt);

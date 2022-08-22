ab85a28cf1e9a978af97017cf3c1467e0263e803
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-22 23:35:33 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	private static int L, C;
+	private static char[] alphabet, code;
+
+	private static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+
+		String[] split = in.readLine().split(" ");
+		// 3 ≤ L ≤ C ≤ 15
+		L = Integer.parseInt(split[0]);
+		C = Integer.parseInt(split[1]);
+
+		code = new char[L];
+
+		split = in.readLine().split(" ");
+		alphabet = new char[C];
+		for (int i = 0; i < C; i++) {
+			alphabet[i] = split[i].charAt(0);
+		}
+		Arrays.sort(alphabet);
+
+		// C 개 중 L개를 선택
+		comb(0, 0, 0);
+
+		System.out.println(sb);
+	}
+	private static void comb(int cnt, int start, int vowelCnt) {
+		// 기저부분
+		if( cnt == L) {
+			if(vowelCnt >= 1 && (L-vowelCnt) >= 2) {
+				sb.append(String.valueOf(code)).append("\n");
+			}
+			return;
+		}
+		// 유도부분
+		for(int i = start; i < C; i++) {
+			code[cnt] = alphabet[i];
+			if(alphabet[i] == 'a'||alphabet[i] == 'e'||alphabet[i] == 'i'||
+					alphabet[i] == 'o'||alphabet[i] == 'u') {
+				comb(cnt+1, i+1, vowelCnt + 1);
+			}else {
+				comb(cnt+1, i+1, vowelCnt);
+			}
+		}
+	}
+}

a8eb290664900fb3d8b06361671f04ba87bc63dc
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-28 00:04:55 +0900

    [Gold IV] Title: ⚾, Time: 708 ms, Memory: 294784 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,23 +1,19 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-// BOJ 17281
-import java.util.Queue;
 
 public class Main {
 	private static int N, maxScore; // N: 이닝 수  N(2 ≤ N ≤ 50)
 	private static int[][] players;
 	private static int[] numbers;
-	private static boolean[] isVisited;
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		N = Integer.parseInt(in.readLine());
+
 		players = new int[N][9];
 
-		// 3번 선수(0 -> 3)는 고정, 나머지 선수 타순 변경 -> 최다 득점 타순 얻기
+		// 3번 선수는 고정, 나머지 선수 타순 변경 -> 최다 득점 타순 얻기
 
 		// 1: 안타, 2: 2루타, 3: 3루타: 4: 4루타, 0: 아웃
 		for (int i = 0; i < N; i++) {
@@ -27,28 +23,20 @@ public class Main {
 			}
 		}
 
-		/**
-		 * 2. 알고리즘 풀기
-		 */
 		// 순열 but, 0번이 3번으로 고정된 상태에서 시작
 		// 야구 타순 결정
 		numbers = new int[9];
-		isVisited = new boolean[9];
 		maxScore = 0;
 
-		perm(0);
+		perm(0, 0);
 	
 
 		// 최고 점수 얻기
 		sb.append(maxScore);
-		/**
-		 * 3. 정답 출력
-		 */
-
 		System.out.println(sb);
 	}
 
-	private static void perm(int cnt) {
+	private static void perm(int cnt, int visited) {
 		// 기저 부분
 		if (cnt == 9) {
 			// 경기 시작!
@@ -57,20 +45,14 @@ public class Main {
 		}
 		// 유도 부분
 		for (int i = 1; i < 9; i++) {
-			if (isVisited[i]) continue;
-
-			// 선택되지 않은 번호
-			isVisited[i] = true;
+			if ((visited & (1 << i)) > 0) continue;
 			numbers[cnt] = i;
 			// 다음 번호
 			if (cnt != 2) {
-				perm(cnt + 1);
+				perm(cnt + 1, visited | (1 << i));
 			}else {
-				perm(cnt + 2);
+				perm(cnt + 2,  visited | (1 << i));
 			}
-			// 선택 취소
-			isVisited[i] = false;
-
 		}
 	}
 

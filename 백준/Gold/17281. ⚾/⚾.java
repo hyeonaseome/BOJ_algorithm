dde9533f1d53bfa92066da8a9073e20f1c724ad8
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2022-08-27 23:56:22 +0900

    [Gold IV] Title: ⚾, Time: 724 ms, Memory: 295132 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 // BOJ 17281
@@ -12,11 +13,11 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-        
+
 		N = Integer.parseInt(in.readLine());
 		players = new int[N][9];
 
-		// 3번 선수는 고정, 나머지 선수 타순 변경 -> 최다 득점 타순 얻기
+		// 3번 선수(0 -> 3)는 고정, 나머지 선수 타순 변경 -> 최다 득점 타순 얻기
 
 		// 1: 안타, 2: 2루타, 3: 3루타: 4: 4루타, 0: 아웃
 		for (int i = 0; i < N; i++) {
@@ -26,13 +27,18 @@ public class Main {
 			}
 		}
 
-		// 순열, 0번이 타순 3번으로 고정된 상태에서 시작
+		/**
+		 * 2. 알고리즘 풀기
+		 */
+		// 순열 but, 0번이 3번으로 고정된 상태에서 시작
 		// 야구 타순 결정
 		numbers = new int[9];
 		isVisited = new boolean[9];
 		maxScore = 0;
 
 		perm(0);
+	
+
 		// 최고 점수 얻기
 		sb.append(maxScore);
 		/**
@@ -67,44 +73,78 @@ public class Main {
 
 		}
 	}
+
 	private static void play() {
 		// 야구 시작
 		int playerNum = 0; // 현재 타자의 번호
-		int outCnt = 0; // 이닝당 3아웃시 이닝 종료
-		
-		Queue<Integer> plates = new ArrayDeque<>();
+		int outCnt = 0; // 1이닝 3아웃시 이닝 종료
+
+		boolean[] plates =  new boolean[4];// 0:홈 1: 1루 2: 2루, 3: 3루
 
 		int score = 0;
 		for (int i = 0; i < N; i++) { //N개의 이닝 소화
-			while(outCnt < 3) {//현 타자의 점수
+			while(outCnt < 3) {
+				//현 타자의 점수
 				int curScore = players[i][numbers[playerNum]];
-				
-				if (curScore == 0) {
+				switch(curScore) {
+				case 0: // 아웃
 					outCnt++;
-				} 
-				else if (curScore == 4) {
-					score += plates.size();
-					score++;
-					plates = new ArrayDeque<>();
-				}
-				else {
-					int size = plates.size();
-					while(--size >= 0) {
-						int poll = plates.poll();
-						if(poll + curScore >= 4) {
-							score++;
-						}else {
-							plates.add(poll + curScore);
+					break;
+				case 1: // 타자와 모든 주자가 한 루씩 진루
+					for (int j = 3; j >= 1; j--) {
+						if(plates[j]) { // 주자가 있다면
+							plates[j] = false;
+							if(j >= 3) {// 득점권이라면
+								score++;
+							}else {
+								plates[j + 1] = true; // 진루
+							}
 						}
 					}
-					plates.add(curScore);
+					// 타자 진루
+					plates[1] = true;
+					break;
+				case 2: // 타자와 모든 주자가 두 루씩 진루
+					for (int j = 3; j >= 1; j--) {
+						if(plates[j]) { // 주자가 있다면
+							plates[j] = false;
+							if(j >= 2) {// 득점권이라면
+								score++;
+							}else {
+								plates[j + 2] = true; // 진루
+							}
+						}
+					}
+					// 타자 진루
+					plates[2] = true;
+					break;
+				case 3: // 타자와 모든 주자가 세 루씩 진루
+					for (int j = 3; j >= 1; j--) {
+						if(plates[j]) { // 주자가 있다면
+							plates[j] = false;
+							score++;
+						}
+					}
+					// 타자 진루
+					plates[3] = true;
+					break;
+				case 4: // 타자와 모든 주자가 홈까지 진루
+					for (int j = 3; j >= 1; j--) {
+						if(plates[j]) { // 주자가 있다면
+							score++;
+						}
+					}
+					// 타자도 점수
+					score++;
+					plates =  new boolean[4];
+					break;
 				}
 
 				// 다음 타자로
 				playerNum = (playerNum + 1) % 9;
 			}
 			outCnt = 0; // 다음 이닝을 위한 outCnt초기화
-			plates = new ArrayDeque<>(); // plates 초기화
+			plates =  new boolean[4];
 		}
 		
 		// 경기끝  최고 점수 갱신

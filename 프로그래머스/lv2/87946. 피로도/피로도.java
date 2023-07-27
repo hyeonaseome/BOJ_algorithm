2e834a97437708934cca3ba51c5bbdc47e9a4b3f
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-07-27 13:21:13 +0900

    [level 2] Title: 피로도, Time: 0.11 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,43 @@
+/*
+최소 필요 피로도: 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도
+소모 피로도: 던전을 탐험한 후 소모되는 피로도
+*/
+import java.util.Arrays;
+class Solution {
+    
+    public int N, maxDungeon;
+    public boolean[] isSelected;
+    
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        
+        N = dungeons.length;
+        maxDungeon = 0;
+        isSelected = new boolean[N];
+        
+        dfs(0, k, dungeons);
+        return maxDungeon;
+    }
+    
+    public void dfs(int cnt, int k, int[][] dungeons) {
+        if( cnt == N) {
+            maxDungeon = Math.max(maxDungeon, cnt);
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            
+            if (isSelected[i]) continue;
+            
+            if (k >= dungeons[i][0]) {
+                isSelected[i] = true;
+                dfs(cnt + 1, k - dungeons[i][1], dungeons);   
+                isSelected[i] = false;
+            }
+            else {
+                maxDungeon = Math.max(maxDungeon, cnt);
+            }
+            
+        }
+    }
+}

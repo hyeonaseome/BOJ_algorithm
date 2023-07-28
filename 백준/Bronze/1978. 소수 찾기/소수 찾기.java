73ff0708a22b79972547b2ab73dcbe1e19c9e692
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-07-28 12:14:29 +0900

    [Bronze II] Title: 소수 찾기, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		// 수의 개수
+		int N = Integer.parseInt(in.readLine());
+		String[] split = in.readLine().split(" ");
+
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			if (checkPrime(Integer.parseInt(split[i]))) {
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+
+	private static boolean checkPrime(int num) {
+
+		if (num == 1)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}

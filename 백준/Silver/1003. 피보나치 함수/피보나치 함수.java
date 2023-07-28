68945ac1fff3a271cf75e92a50a698efe57ed0ee
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-07-28 16:35:12 +0900

    [Silver III] Title: 피보나치 함수, Time: 72 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -7,17 +7,19 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
+		int num = 40;
+		memo = new int[num + 1][3];
+		for (int i = 0; i <= num; i++) {
+			memo[i][0] = -1;
+			memo[i][1] = -1;
+		}
+		fibonacci(num);
 
 		int T = Integer.parseInt(in.readLine());
 		for (int t = 1; t <= T; t++) {
-			int num = Integer.parseInt(in.readLine());
-			memo = new int[num + 1][3];
-			for (int i = 0; i <= num; i++) {
-				memo[i][0] = -1;
-				memo[i][1] = -1;
-			}
-			fibonacci(num);
-			sb.append(memo[num][0]).append(" ").append(memo[num][1]).append("\n");
+			int n = Integer.parseInt(in.readLine());
+			sb.append(memo[n][0]).append(" ").append(memo[n][1]).append("\n");
 		}
 
 		System.out.println(sb);

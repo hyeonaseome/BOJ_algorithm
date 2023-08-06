a14b8953c076341f241837160facf9d2995fd6bc
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-06 11:44:38 +0900

    [level 2] Title: 더 맵게, Time: 1934.90 ms, Memory: 313 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/README.md
+++ 프로그래머스/lv2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 318 MB, 시간: 1898.86 ms
+메모리: 313 MB, 시간: 1934.90 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -9,34 +9,29 @@ class Solution {
         }
         int res =0;
         while(true) {
-            // map size가 1일 때
             if(map.size()==1) {
                 break;
             }
-            // map.size가 2 이상
             int cnt =0;
             long[] arr = new long[2];
             for(long x : map.keySet()) {
                 int len = Math.min(2 - cnt, map.get(x));
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < len; i ++)  {
                     arr[cnt++] = x;
                 }
-                
                 if(cnt==2) {
                     break;
                 }
-
             }
-
             if(arr[0]>=K) {
                 break;
             }
             map.put(arr[0],map.get(arr[0])-1);
-            if(map.get(arr[0]) ==0) {
+            if(map.get(arr[0])==0) {
                 map.remove(arr[0]);
             }
             map.put(arr[1],map.get(arr[1])-1);
-            if(map.get(arr[1]) == 0) {
+            if(map.get(arr[1])==0) {
                 map.remove(arr[1]);
             }
             long x = arr[0]+arr[1]*2;

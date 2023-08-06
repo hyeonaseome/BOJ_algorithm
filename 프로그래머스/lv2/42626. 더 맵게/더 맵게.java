2c66f94feeb68fd15816153b3c377bb623e51df6
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-06 10:51:32 +0900

    [level 2] Title: 더 맵게, Time: 1743.21 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+        
+        while(true) {
+            if (pq.peek() >= K) {
+                break;
+            }
+            
+            if (pq.size() == 1) {
+                answer = -1;
+                break;
+            }
+            answer++;
+            int scoville1 = pq.poll();
+            int scoville2 = pq.poll();
+            pq.add(scoville1 + scoville2 * 2);
+            
+        }
+        return answer;
+    }
+}

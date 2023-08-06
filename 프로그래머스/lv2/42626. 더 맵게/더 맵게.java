ec278e7bfcb44b1a8970f1c4d3219431cd3bd3c2
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-06 11:39:23 +0900

    [level 2] Title: 더 맵게, Time: 1898.86 ms, Memory: 318 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,25 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < scoville.length; i++) {
-            pq.add(scoville[i]);
+        SortedMap<Long,Integer> map = new TreeMap<>();
+        for(long x : scoville) {
+            map.put(x,map.getOrDefault(x,0)+1);
         }
-        
-        while(pq.peek() < K) {
-            if (pq.size() == 1) {
-                answer = -1;
+        int res =0;
+        while(true) {
+            // map size가 1일 때
+            if(map.size()==1) {
                 break;
             }
-            answer++;
-            
-            int scoville1 = pq.poll();
-            int scoville2 = pq.poll();
-            pq.add(scoville1 + scoville2 * 2);
+            // map.size가 2 이상
+            int cnt =0;
+            long[] arr = new long[2];
+            for(long x : map.keySet()) {
+                int len = Math.min(2 - cnt, map.get(x));
+                for (int i = 0; i < len; i++) {
+                    arr[cnt++] = x;
+                }
+                
+                if(cnt==2) {
+                    break;
+                }
+
+            }
+
+            if(arr[0]>=K) {
+                break;
+            }
+            map.put(arr[0],map.get(arr[0])-1);
+            if(map.get(arr[0]) ==0) {
+                map.remove(arr[0]);
+            }
+            map.put(arr[1],map.get(arr[1])-1);
+            if(map.get(arr[1]) == 0) {
+                map.remove(arr[1]);
+            }
+            long x = arr[0]+arr[1]*2;
+            map.put(x, map.getOrDefault(x,0) +1);
+
+            res++;
         }
-        return answer;
+        if(map.firstKey()>=K){
+            return res;
+        }
+        return -1;
     }
 }

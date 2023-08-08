1508d92eeaa2cbcd7606ac9336d2ae7ec36d06a0
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-08 14:41:46 +0900

    [Silver I] Title: 곱셈, Time: 232 ms, Memory: 18620 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -3,26 +3,24 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] nums = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		long[] nums = Arrays.stream(in.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
 
 		System.out.println(pow(nums[0], nums[1], nums[2]));
 	}
 
-	private static long pow(int A, int B, int C) {
-		if (B == 1)
-			return A % C;
-		else {
-			long x = pow(A, B/2, C) % C;
-			if (B % 2 == 0) {
-				return x * x % C;
-			} else {
-				return (x * x % C) * A % C;
+	private static long pow(long A, long B, long C) {
+		long res = 1;
+		while(B > 0) {
+			if ((B & 1) == 1) {
+				res = ((res % C) * (A % C)) % C;
 			}
+			A = ((A % C) * (A % C)) % C;
+			B = B >> 1;
 		}
+		return res % C;
 	}
 
 }

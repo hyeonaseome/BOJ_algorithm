ecff180b68e03f0439eb0ba8dd39fdde19bb5f00
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-11 10:54:38 +0900

    [unrated] Title: 글자 지우기, Time: 0.09 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder(my_string);
+        for (int i = 0; i < indices.length; i++) {
+            sb.setCharAt(indices[i], '_');
+        }
+
+        return sb.toString().replace("_", "");
+    }
+}

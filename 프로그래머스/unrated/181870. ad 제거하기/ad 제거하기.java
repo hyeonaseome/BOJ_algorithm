b6324500eb6a6712b0d32f9da02953f47ea742d2
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-17 23:11:41 +0900

    [unrated] Title: ad 제거하기, Time: 1.09 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        String[] answer = {};
+        
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < strArr.length; i++) {
+            if (!strArr[i].contains("ad")) {
+                list.add(strArr[i]);
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+}

f7793b3749dc32c9e73bb8ccf6ac2eeb8a0cda9c
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-23 15:16:23 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for (int num : num_list) {
+            if (num == n) {
+                return 1;
+            }
+        }
+        
+        return answer;
+    }
+}

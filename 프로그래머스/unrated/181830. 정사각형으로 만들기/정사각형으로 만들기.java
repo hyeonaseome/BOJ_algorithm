5d425a29ecca0bf5991c379b593942ca5c33f0a3
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-25 01:16:32 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.26 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        int max = n > m ? n : m;
+        int[][] answer = new int[max][max];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                answer[i][j] = arr[i][j];
+            }
+        }
+        return answer;
+    }
+}

675863acc494efa42720f56a0a67f3d6b908fd28
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-08-31 21:39:06 +0900

    [level 3] Title: 숫자 게임, Time: 66.03 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int j = B.length - 1;
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (j < 0) {
+                break;
+            }
+            if (A[i] < B[j]) {
+                answer++;
+                j--;
+            }
+            
+        }
+        return answer;
+    }
+}

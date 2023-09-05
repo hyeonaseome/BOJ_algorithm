9de80306f2e2d1d1f8e8653c7ccfb92f6756986f
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2023-09-06 00:37:07 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 1;
+        int answer = 0;
         Queue<Integer> bridge = new ArrayDeque();
         int truckSum = 0;
         
-        for (int i = 0; i < bridge_length - 1; i++) {
+        for (int i = 0; i < bridge_length; i++) {
             bridge.add(0);
         }
-        bridge.add(truck_weights[0]);
-        truckSum += truck_weights[0];
         
-        for (int i = 1; i < truck_weights.length; i++) {
+        for (int i = 0; i < truck_weights.length; i++) {
             int cur = truck_weights[i];
             while (truckSum - bridge.peek() + cur > weight) {
                 truckSum -= bridge.poll();

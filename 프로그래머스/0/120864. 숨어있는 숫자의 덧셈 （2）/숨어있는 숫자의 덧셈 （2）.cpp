2bcce9be7ee5e64fee9c4f3d924c416bd09f5d2f
Hyeona Seo <109887404+hyeonaseome@users.noreply.github.com>
2024-04-26 01:58:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -4,19 +4,12 @@
 
 using namespace std;
 
-bool isDigit(char s) {
-    if ('0' <= s && s <= '9') {
-        return true;
-    }
-    return false;
-}
-
 int solution(string my_string) {
     int answer = 0;
     
     int temp = 0;
     for (auto s : my_string) {
-        if (isDigit(s)) {
+        if (isdigit(s)) {
             temp = temp * 10 + s - '0';
         } else {
             answer += temp;
